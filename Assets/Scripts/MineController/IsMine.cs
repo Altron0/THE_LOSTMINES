a24a7d19c8e7db37cee2b49794cfe10e1a1afbbd
MAKS <mitenin101@mail.ru>
2025-02-18 22:34:31 +0300

    Рандомный цвет шахт, начало работы с джейсоном

---
+++ Assets/Scripts/MineController/IsMine.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsMine : MonoBehaviour
+{
+    List<Material> getMaterial()
+    {
+        TryGetComponent(out MeshRenderer renderer);
+
+        List<Material> mt = new List<Material>();
+        for(int i = 0;i < renderer.materials.Length;i++)
+        {
+            mt.Add(renderer.materials[i]);
+        }
+        return mt;
+    }
+
+
+    void setMaterial(List<Material> mt)
+    {
+        TryGetComponent(out MeshRenderer renderer);
+        renderer.SetMaterials(mt);
+    }
+}
